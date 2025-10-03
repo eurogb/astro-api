5997aa3881b1bc20cdd15ed9d4af673afb85f92d
eurogb <rio1zamet@gmail.com>
2025-10-03 18:28:29 +0200

    Update tipHr.js

--- api/tipHr.js
+++ api/tipHr.js
@@ -3,7 +3,12 @@ import path from 'path';
 
 export default function handler(req, res) {
   const filePath = path.join(process.cwd(), 'data', 'tip', 'tip.hr.json');
-  const fileContent = fs.readFileSync(filePath, 'utf8');
-  const jsonData = JSON.parse(fileContent);
-  res.status(200).json(jsonData);
+
+  try {
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    const jsonData = JSON.parse(fileContent);
+    res.status(200).json(jsonData);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to load tip.hr.json' });
+  }
 }
