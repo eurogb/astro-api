d64d6be0c4885a253cf839cc6d35cd2e60d98a64
eurogb <rio1zamet@gmail.com>
2025-10-04 23:57:43 +0200

    Update evaluate.js

--- api/evaluate.js
+++ api/evaluate.js
@@ -5,8 +5,10 @@ export const config = {
 };
 
 export default async function handler(req, res) {
+  const allowedOrigin = "https://eurogb.github.io";
+
   // 🌐 CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-quiz-token");
 
