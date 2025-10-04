022c1ed5ba6066061bb39a8e99dda59e2ebf926f
eurogb <rio1zamet@gmail.com>
2025-10-04 19:11:14 +0200

    Update tipHr.js

--- api/tipHr.js
+++ api/tipHr.js
@@ -10,3 +10,13 @@ export default function handler(req, res) {
   const tip = tips[Math.floor(Math.random() * tips.length)];
   res.status(200).json({ tip });
 }
+
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    const jsonData = JSON.parse(fileContent);
+
+    res.status(200).json(jsonData);
+  } catch (err) {
+    console.error("tipHr error:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
